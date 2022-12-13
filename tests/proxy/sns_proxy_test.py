dba204f03289dd7311c2c6455032a4abc6db1db0
Stephen Terrell <stephen.terrell14@gmail.com>
2022-12-12 20:29:28 -0800

    fix broken test

--- tests/proxy/sns_proxy_test.py
+++ tests/proxy/sns_proxy_test.py
@@ -11,7 +11,7 @@ def test_simple_assert():
 
 @patch.dict(os.environ, {'AWS_REGION': 'us-west-2', 'AWS_DEFAULT_REGION': 'us-west-2', 'AWS_ACCOUNT_ID': '1234'})
 @patch("src.proxy.sns_proxy.boto3")
-def test_me_is_test(boto3_mock, os_environ_patch):
+def test_me_is_test(boto3_mock):
     under_test_2 = SnsProxy()
 
     asdf = 'asdf'
